f67b6e38eb636d777aff777640c7126c32cfc428
Hamam <zero.saturnus@gmail.com>
2021-10-02 21:04:06 +0700

    feature/intro_screen : add uitls color

---
+++ src/utils/colors/index.js
@@ -0,0 +1,23 @@
+const mainColors = {
+  blue: '#148CC7',
+  blue2: '#004087',
+  grey1: '#969A99',
+  grey2: '#DDDDDD',
+  black: '#000',
+  white: '#ffffff',
+};
+
+export const colors = {
+  button: {
+    tertiary: mainColors.blue,
+  },
+  background: {
+    main: mainColors.white,
+    secondary: mainColors.blue,
+  },
+  text: {
+    white: mainColors.white,
+    blue: mainColors.blue,
+    blue2: mainColors.blue2,
+  },
+};
