9ef8c62a6175dcc5100ed0fbe487eaa7a4615905
sularso17771 <sularso.moch@gmail.com>
2022-01-21 10:41:48 +0700

    feature/home: revisi register

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,166 +1,94 @@
-import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import {
-  Image,
-  Pressable,
-  StatusBar,
-  Text,
-  TextInput,
-  View,
-} from 'react-native';
-import { facebookIcon, googleIcon, LogoYuPeriksa } from '../../assets';
-import * as Font from '../../assets/fonts/index';
+import { Pressable, View } from 'react-native';
+import { Divider } from 'react-native-elements';
+import * as constanta from '../../assets';
+import { Input, TextCustom } from '../../components/atoms';
+import { TemplateLogin } from '../../components/organism';
 import { colors } from '../../utils/colors';
 
 const Register = ({ navigation }) => {
-  const renderLogo = () => {
-    return (
-      <Image
-        source={LogoYuPeriksa}
-        resizeMode="contain"
-        style={{ width: 150, height: 54, marginVertical: 43 }}
-      />
-    );
-  };
-
-  const renderForm = () => {
+  const TextFooter = () => {
     return (
       <View
         style={{
-          width: '100%',
-          height: '100%',
-          backgroundColor: colors.background.main,
-          borderTopRightRadius: 30,
-          borderTopLeftRadius: 30,
-          padding: 40,
+          flexDirection: 'row',
+          height: 35,
+          alignContent: 'center',
         }}>
-        <StatusBar backgroundColor={colors.background.secondary} />
-        <Text
+        <TextCustom
+          text={'Anda Lupa Password ? '}
           style={{
-            fontFamily: Font.FAMILY_BOLD,
-            fontSize: Font.FONTSIZE_LG,
-            color: colors.text.blue,
-            marginBottom: 10,
-          }}>
-          Daftar
-        </Text>
-        <TextInput
-          style={{
-            height: 45,
-            width: '100%',
-            borderRadius: 10,
-            borderColor: colors.border,
-            borderWidth: 1,
-            paddingHorizontal: 15,
-            marginVertical: 20,
+            color: colors.text.forth,
           }}
-          placeholder={'Masukan Email Anda'}
-        />
-
-        <TextInput
-          style={{
-            height: 45,
-            width: '100%',
-            borderRadius: 10,
-            borderColor: colors.border,
-            borderWidth: 1,
-            paddingHorizontal: 15,
-          }}
-          placeholder={'Masukan No Telpon Anda'}
-        />
-
-        <TextInput
-          style={{
-            height: 45,
-            width: '100%',
-            borderRadius: 10,
-            borderColor: colors.border,
-            borderWidth: 1,
-            paddingHorizontal: 15,
-            marginVertical: 20,
-          }}
-          placeholder={'Masukan Password'}
-        />
-
-        <TextInput
-          style={{
-            height: 45,
-            width: '100%',
-            borderRadius: 10,
-            borderColor: colors.border,
-            borderWidth: 1,
-            paddingHorizontal: 15,
-            marginBottom: 20,
-          }}
-          placeholder={'Masukan Password Konfirmasi'}
         />
         <Pressable
-          style={{
-            backgroundColor: colors.button.background1,
-            borderRadius: 15,
-            height: 45,
-            width: '100%',
-            justifyContent: 'center',
+          onPress={() => {
+            navigation.navigate('ForgotPassword');
           }}>
-          <Text
-            style={{
-              alignSelf: 'center',
-              color: colors.text.white,
-              fontFamily: Font.FAMILY_BOLD,
-              fontSize: Font.FONTSIZE_MD,
-            }}>
-            DAFTAR
-          </Text>
+          <TextCustom text={'Reset'} />
         </Pressable>
-        <View
+        <TextCustom
+          text={'atau'}
           style={{
-            height: 1,
-            width: '100%',
-            backgroundColor: colors.border,
-            marginVertical: 25,
+            color: colors.text.forth,
+            marginHorizontal: 5,
           }}
         />
-        <View
-          style={{
-            flexDirection: 'row',
-            alignContent: 'center',
+        <Pressable
+          onPress={() => {
+            navigation.navigate('Login');
           }}>
-          <Text
-            style={{
-              fontFamily: Font.FAMILY,
-              fontSize: Font.FONTSIZE_MD,
-              color: colors.text.forth,
-            }}>
-            {'Sudah punya akun '}
-          </Text>
-          <Pressable
-            onPress={() => {
-              navigation.navigate('Login');
-            }}>
-            <Text
-              style={{
-                fontFamily: Font.FAMILY,
-                fontSize: Font.FONTSIZE_MD,
-                color: colors.text.tertiery,
-              }}>
-              Login
-            </Text>
-          </Pressable>
-        </View>
+          <TextCustom text={'Login'} />
+        </Pressable>
       </View>
     );
   };
 
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: colors.background.secondary,
-        alignItems: 'center',
-      }}>
-      {renderLogo()}
-      {renderForm()}
-    </View>
+    <TemplateLogin>
+      <TextCustom
+        text={'DAFTAR'}
+        style={{
+          marginBottom: 20,
+          fontSize: constanta.FONTSIZE_MD,
+          color: colors.text.secondary,
+          fontFamily: constanta.FAMILY_BOLD,
+        }}
+      />
+      <Input
+        placeholder={'Masukan Nama Lengkap'}
+        style={{ marginBottom: 20 }}
+      />
+      <Input placeholder={'Masukan E-mail'} style={{ marginBottom: 20 }} />
+      <Input placeholder={'Masukan No Telp'} style={{ marginBottom: 20 }} />
+      <Input placeholder={'Masukan Password'} style={{ marginBottom: 20 }} />
+      <Input
+        placeholder={'Masukan Password Konfirmasi'}
+        style={{ marginBottom: 20 }}
+      />
+      <Pressable
+        style={{
+          backgroundColor: colors.text.orange,
+          height: 40,
+          borderRadius: 10,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <TextCustom
+          text={'DAFTAR'}
+          style={{
+            color: colors.text.white,
+            fontFamily: constanta.FAMILY_BOLD,
+          }}
+        />
+      </Pressable>
+      <Divider
+        width={1}
+        color={colors.text.grey2}
+        style={{ marginVertical: 20 }}
+      />
+      {TextFooter()}
+    </TemplateLogin>
   );
 };
 
