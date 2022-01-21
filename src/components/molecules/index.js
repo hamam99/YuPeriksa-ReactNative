4d8e88a545fb3903ba4f06b56ab8b1a50a6d5075
sularso17771 <sularso.moch@gmail.com>
2022-01-21 10:40:26 +0700

    feature/home: add component header logo

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -2,5 +2,6 @@ import CardDigital from './cards/CardDigital';
 import CardFeature from './cards/CardFeature';
 import Wrap from './wraps/Wrap';
 import ListSchedule from './lists/ListSchedule';
+import HeaderLogo from './header/HeaderLogo';
 
-export { CardDigital, CardFeature, Wrap, ListSchedule };
+export { CardDigital, CardFeature, Wrap, ListSchedule, HeaderLogo };
