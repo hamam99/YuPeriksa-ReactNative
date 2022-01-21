6d90a9db5969c0ea184f15fb7882072ed0a6711e
sularso17771 <sularso.moch@gmail.com>
2022-01-21 10:41:04 +0700

    feature/home: revisi ForgotPassword

--- src/pages/ForgotPassword/index.js
+++ src/pages/ForgotPassword/index.js
@@ -1,130 +1,66 @@
 import React from 'react';
-import {
-  Image,
-  Pressable,
-  StatusBar,
-  Text,
-  TextInput,
-  View,
-} from 'react-native';
-import { LogoYuPeriksa } from '../../assets';
-import * as Font from '../../assets/fonts/index';
+import { Pressable, View, Image, Text, StyleSheet } from 'react-native';
+import { Divider } from 'react-native-elements';
+import * as Font from '../../assets';
+import { Input, TextCustom } from '../../components/atoms';
+import { TemplateLogin } from '../../components/organism';
 import { colors } from '../../utils/colors';
+import { facebookIcon, googleIcon, LogoYuPeriksa } from '../../assets';
 
 const ForgotPassword = ({ navigation }) => {
-  const renderLogo = () => {
-    return (
-      <Image
-        source={LogoYuPeriksa}
-        resizeMode="contain"
-        style={{ width: 150, height: 54, marginVertical: 43 }}
-      />
-    );
-  };
+  const styles = StyleSheet.create({
+    pressable: {
+      borderRadius: 15,
+      flex: 1,
+      justifyContent: 'center',
+      borderWidth: 1,
+      borderColor: colors.border,
+    },
+    iconGoogle: {
+      width: 20,
+      height: 20,
+      alignSelf: 'center',
+      marginRight: 21,
+      position: 'absolute',
+      left: 0,
+      marginLeft: 13,
+    },
+    iconFacebook: {
+      width: 30,
+      height: 30,
+      alignSelf: 'center',
+      marginRight: 21,
+      position: 'absolute',
+      left: 0,
+    },
+    containerFooter: {
+      flexDirection: 'column',
+      alignContent: 'center',
+    },
+  });
 
-  const renderForm = () => {
+  const TextFooter = () => {
     return (
-      <View
-        style={{
-          width: '100%',
-          height: '100%',
-          backgroundColor: colors.background.main,
-          borderTopRightRadius: 30,
-          borderTopLeftRadius: 30,
-          padding: 40,
-        }}>
-        <StatusBar backgroundColor={colors.background.secondary} />
-        <Text
-          style={{
-            fontFamily: Font.FAMILY_BOLD,
-            fontSize: Font.FONTSIZE_LG,
-            color: colors.text.blue,
-            marginBottom: 20,
-          }}>
-          Lupa Password
-        </Text>
-        <TextInput
-          style={{
-            height: 45,
-            width: '100%',
-            borderRadius: 10,
-            borderColor: colors.border,
-            borderWidth: 1,
-            paddingHorizontal: 15,
-            marginBottom: 20,
-          }}
-          placeholder={'Masukan Email Anda'}
-        />
-
-        <Pressable
-          onPress={() => {
-            navigation.navigate('Main');
-          }}
-          style={{
-            backgroundColor: colors.button.background1,
-            borderRadius: 15,
-            height: 45,
-            width: '100%',
-            justifyContent: 'center',
-          }}>
-          <Text
-            style={{
-              alignSelf: 'center',
-              color: colors.text.white,
-              fontFamily: Font.FAMILY_BOLD,
-              fontSize: Font.FONTSIZE_MD,
-            }}>
-            LUPA PASSWORD
-          </Text>
-        </Pressable>
-        <View
-          style={{
-            height: 1,
-            width: '100%',
-            backgroundColor: colors.border,
-            marginVertical: 25,
-          }}
-        />
-        <View
-          style={{
-            flexDirection: 'row',
-            height: 35,
-            alignContent: 'center',
-          }}>
+      <View style={styles.containerFooter}>
+        <View style={{ flexDirection: 'row' }}>
           <Pressable
             onPress={() => {
-              navigation.navigate('Login');
+              navigation.navigate('ForgotPassword');
             }}>
-            <Text
-              style={{
-                fontFamily: Font.FAMILY,
-                fontSize: Font.FONTSIZE_MD,
-                color: colors.text.tertiery,
-              }}>
-              Login
-            </Text>
+            <TextCustom text={'Login'} />
           </Pressable>
-          <Text
+          <TextCustom
+            text={'atau'}
             style={{
-              fontFamily: Font.FAMILY,
-              fontSize: Font.FONTSIZE_MD,
               color: colors.text.forth,
               marginHorizontal: 5,
-            }}>
-            atau
-          </Text>
+            }}
+          />
           <Pressable
             onPress={() => {
               navigation.navigate('Register');
             }}>
-            <Text
-              style={{
-                fontFamily: Font.FAMILY,
-                fontSize: Font.FONTSIZE_MD,
-                color: colors.text.tertiery,
-              }}>
-              Daftar
-            </Text>
+            <TextCustom text={'Daftar'} />
           </Pressable>
         </View>
       </View>
@@ -132,15 +68,40 @@ const ForgotPassword = ({ navigation }) => {
   };
 
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: colors.background.secondary,
-        alignItems: 'center',
-      }}>
-      {renderLogo()}
-      {renderForm()}
-    </View>
+    <TemplateLogin>
+      <TextCustom
+        text={'LUPA PASSWORD'}
+        style={{
+          marginBottom: 20,
+          fontSize: Font.FONTSIZE_MD,
+          color: colors.text.secondary,
+          fontFamily: Font.FAMILY_BOLD,
+        }}
+      />
+      <Input placeholder={'Masukan E-mail'} style={{ marginBottom: 20 }} />
+      <Pressable
+        style={{
+          backgroundColor: colors.text.orange,
+          height: 40,
+          borderRadius: 10,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <TextCustom
+          text={'LUPA PASSWORD'}
+          style={{
+            color: colors.text.white,
+            fontFamily: Font.FAMILY_BOLD,
+          }}
+        />
+      </Pressable>
+      <Divider
+        width={1}
+        color={colors.text.grey2}
+        style={{ marginVertical: 20 }}
+      />
+      {TextFooter()}
+    </TemplateLogin>
   );
 };
 
