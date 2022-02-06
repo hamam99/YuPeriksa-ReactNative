9196e89c7abb1480e9cc50c7be243641b3766016
Hamam <zero.saturnus@gmail.com>
2022-02-06 16:29:22 +0700

    feature/layanan_umum : 1. Add header 2. add divider

--- src/pages/Layanan/LayananUmum/index.js
+++ src/pages/Layanan/LayananUmum/index.js
@@ -1,8 +1,50 @@
 import React from 'react';
-import { Text, } from 'react-native';
+import { Alert, Text, View } from 'react-native';
+// import { Icon } from 'react-native-elements/dist/icons/Icon';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const LayananUmum = () => {
-  return <Text>Layanan Umum</Text>;
+  const Header = () => {
+    return (
+      <View
+        style={{
+          width: '100%',
+          height: 58,
+          alignItems: 'center',
+          marginHorizontal: 20,
+          flexDirection: 'row',
+        }}>
+        <Icon size={20} name="chevron-back" onPress={() => {}} />
+        <Text
+          style={{
+            fontSize: 16,
+          }}>
+          Umum
+        </Text>
+      </View>
+    );
+  };
+
+  const Divider = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: '100%',
+          backgroundColor: '#E2E2E2',
+          marginHorizontal: 20,
+        }}
+      />
+    );
+  };
+  return (
+    <View style={{ flex: 1 }}>
+      <Header />
+      <Divider />
+
+      <Text>Layanan Umum</Text>
+    </View>
+  );
 };
 
 export default LayananUmum;
