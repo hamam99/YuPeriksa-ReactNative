da420f29e932f95d7d7d70109ea217f5bee1c38e
Hamam <zero.saturnus@gmail.com>
2022-02-06 20:10:24 +0700

    feature/layanan_umum : set action menu filter

--- src/pages/Layanan/LayananUmum/index.js
+++ src/pages/Layanan/LayananUmum/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import { Alert, Text, ToastAndroid, View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, Text, ToastAndroid, View } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FontAwesomeIcons from 'react-native-vector-icons/FontAwesome';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { styles } from './styles';
 
 const LayananUmum = () => {
+  const [idMenuFilterActive, setIdMenuFilterActive] = useState(0);
+
   const Header = () => {
     const onPressBack = () => {
       ToastAndroid.show('Back', ToastAndroid.SHORT);
@@ -32,11 +34,69 @@ const LayananUmum = () => {
     return <View style={styles.dividerHeader} />;
   };
 
+  const renderItem = ({ item }) => {
+    const isActive = idMenuFilterActive === item.id;
+
+    return (
+      <TouchableOpacity
+        style={{
+          marginLeft: 14,
+          width: 140,
+          height: 35,
+          backgroundColor: isActive ? '#FFEDCE' : '#ffffff',
+          borderRadius: 15,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        onPress={() => {
+          setIdMenuFilterActive(item.id);
+        }}>
+        <Text style={{ color: isActive ? '#FF972E' : '#969A99' }}>
+          {item.title}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+
+  const MenuFilter = () => {
+    console.log('MenuFilter');
+    const filterMenu = [
+      {
+        id: 1,
+        title: 'Klinik',
+      },
+      {
+        id: 2,
+        title: 'Puskesmas',
+      },
+      {
+        id: 3,
+        title: 'Kebidanan',
+      },
+      {
+        id: 4,
+        title: 'Perawatan Tubuh',
+      },
+    ];
+
+    return (
+      <View style={{ paddingVertical: 20 }}>
+        <FlatList
+          data={filterMenu}
+          keyExtractor={item => item.id}
+          renderItem={renderItem}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
       <Header />
       <Divider />
-      <Text style={{ marginHorizontal: 20 }}>Layanan Umum</Text>
+      <MenuFilter />
     </View>
   );
 };
