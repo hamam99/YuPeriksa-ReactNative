cd6ac53ce5104958933fafa3d1c9e136104625d8
Hamam <zero.saturnus@gmail.com>
2022-03-02 14:56:56 +0700

    change title

--- src/components/atoms/Header/index.js
+++ src/components/atoms/Header/index.js
@@ -4,7 +4,7 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import { styles } from './styles';
 import FontAwesomeIcons from 'react-native-vector-icons/FontAwesome';
 
-const Header = () => {
+const Header = ({ title }) => {
   const onPressBack = () => {
     ToastAndroid.show('Back', ToastAndroid.SHORT);
   };
@@ -18,7 +18,7 @@ const Header = () => {
       <TouchableOpacity onPress={onPressBack}>
         <Ionicons size={20} name="chevron-back" />
       </TouchableOpacity>
-      <Text style={styles.titleHeader}>Umum</Text>
+      <Text style={styles.titleHeader}>{title}</Text>
       <TouchableOpacity onPress={onPressFilter} style={styles.filterHeader}>
         <FontAwesomeIcons size={20} color="#969A99" name="filter" />
       </TouchableOpacity>
