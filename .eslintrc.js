4f30dc2cbf59974055c963cbaf06124d13a5b7f2
Hamam <zero.saturnus@gmail.com>
2022-03-04 14:01:27 +0700

    convert to ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'react-hooks/exhaustive-deps': 'off',
+    'react-native/no-inline-styles': 'off',
+    curly: 'off',
+    eqeqeq: 'off',
+    'no-shadow': 'warn',
+    'no-unreachable': 'warn',
+    'no-alert': 'off',
+    'react-hooks/rules-of-hooks': 'warn',
+  },
 };
--- src/router/index.js
+++ src/router/index.js
@@ -31,7 +31,7 @@ const Router = () => {
         close: config,
       },
     }}
-    initialRouteName="HospitalDetail">
+    initialRouteName="Intro">
       <Stack.Screen
         name="Intro"
         component={Intro}
